0d189296ea123938c9a93bced1e4fea89806c548
FranciscoGontijo <franciscoacmg@gmail.com>
2022-12-15 15:56:00 +1100

    Track component HTML and CSS

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Track.css';
+
+export class Track extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    renderAction () {
+        if (this.props.isRemoval === false) {
+            return '+'
+        } else {
+            return '-'
+        };
+    }
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3></h3>
+                    <p></p>
+                </div>
+                <button className="Track-action">{this.renderAction()}</button>
+            </div>
+        );
+    }
+};
