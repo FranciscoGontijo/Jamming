859c6c138c47e0f7e9f0622047ab15daaa5cf6a4
FranciscoGontijo <franciscoacmg@gmail.com>
2022-12-15 19:02:06 +1100

    Add tracks to Playlist method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,13 +8,26 @@ import Playlist from '../Playlist/Playlist';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = { 
       searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2',
       id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
       playlistName: 'playlist',
       playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
     };
+
+    this.addTrack = this.addTrack.bind(this);
   }
+
+  addTrack(track) {
+    let playlist = this.state.playlistTracks;
+    if (playlist.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    playlist.push(track);
+    this.setState({ playlistTracks: playlist });
+  }
+
   render() {
     return (
       <div>
@@ -22,7 +35,7 @@ class App extends React.Component {
           <div className="App">
             <SearchBar />
             <div className="App-playlist">
-              <SearchResults searchResults={this.state.searchResults} />
+              <SearchResults onAdd={this.addTrack} searchResults={this.state.searchResults} />
               <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
             </div>
           </div>
